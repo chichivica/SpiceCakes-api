6edb4e44349a88074a2cd5baa1aec3fca6a07aba
Ivan Talalaev <talalaev.i.a@gmail.com>
2017-02-13 15:45:46 +0300

    api documentation

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "SpiceCakes-api",
+  "version": "0.1.0",
+  "description": "api documentation",
+  "title": "SpiceCakes api documentation",
+  "url": "https://github.com/chichivica/SpiceCakes-api/tree/master/apidoc",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-02-13T12:44:10.105Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
