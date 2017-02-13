8570b16ef9b6069106e443a5826733bb308d0454
Ivan Talalaev <talalaev.i.a@gmail.com>
2017-02-13 12:54:19 +0300

    simple server

---
+++ app.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Ivan Talalaev on 2/13/2017.
+ */
+let express = require('express'),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser');
+
+
+let routes = require('./routes/index'),
+  orders = require('./routes/orders');
+
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', orders);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: (app.get('env') === 'development') ? err : {}
+  });
+});
+
+
+module.exports = app;
+
